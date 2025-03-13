e2cdebcb81c86aa1ece53179d65d45e20595096b
Ruben <ruben@bitcube.tech>
2025-03-13 23:17:05 +0200

    update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,8 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 curl  -X POST \
   'http://127.0.0.1:5000/' \
   --header 'Accept: */*' \
   --header 'User-Agent: Thunder Client (https://www.thunderclient.com)' \
   --header 'Content-Type: text/plain' \
-  -d @test.html
+  -d @$SCRIPT_DIR/test.html
