5d19a6dbc27d97459ad3b091293f5d7b326bea84
Ruben <ruben@bitcube.tech>
2025-03-13 20:15:31 +0200

    Download random wiki pages

--- ai/scrappy.py
+++ ai/scrappy.py
@@ -20,9 +20,9 @@ def links_from_booking_com(file, output, cssClass):
         for url in urls:
             f.write(url + "\n")
 
-def save_html(url):
+def save_html(url, folder):
     try:
-        fileName = dir_path + "/raw_hotel/" + url.rsplit('/', 1)[-1]
+        fileName = folder + "/" + url.rsplit('/', 1)[-1]
         if (not os.path.isfile(fileName)):    
             with open(fileName, "x") as f:
                 response = requests.get(url, timeout=10)
@@ -31,7 +31,11 @@ def save_html(url):
         print(f"Failed to extract {url}: {e}")
         return None
 
-def download_pages(link_file):
+def download_pages(link_file, save_dir):
+    # Create the save_dir if it doesn't exists
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    
     # Read URLs from file
     with open(link_file) as f:
         urls = [line.strip() for line in f.readlines()]
@@ -39,17 +43,45 @@ def download_pages(link_file):
 
     # Run in parallel
     with ThreadPoolExecutor(max_workers=20) as executor:  # Adjust max_workers as needed
-        futures = {executor.submit(save_html, url): url for url in urls}
+        futures = {executor.submit(save_html, url, save_dir): url for url in urls}
         
         # Track progress
         for i, future in enumerate(futures):
             future.result()
             printProgressBar(i + 1, total)
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+current_dir = os.path.dirname(os.path.realpath(__file__))
 
-urls_file_name = dir_path + "/uk-booking-urls.txt"
-links_from_booking_com(dir_path + "/booking.com-uk-hotels.mhtml", urls_file_name, "a78ca197d0")  
-download_pages(urls_file_name)
+urls_file_name = current_dir + "/uk-booking-urls.txt"
+links_from_booking_com(current_dir + "/booking.com-uk-hotels.mhtml", urls_file_name, "a78ca197d0")  
+download_pages(urls_file_name, current_dir + "/raw_hotel")
+
+def get_redirect_url(url):
+    try:
+        response = requests.get(url, allow_redirects=True, timeout=5)
+        print(response.url)
+        return response.url
+    except requests.RequestException as e:
+        return f"Error: {e}"
 
 # Grab random wiki articles
+def grab_random_wiki_article():    
+    cache_file = current_dir + "/wiki-links.txt"
+    # If we don't have a wiki links, make a new one
+    if not os.path.isfile(cache_file):
+        total = 500
+        links = []
+        # Get links
+        with ThreadPoolExecutor(max_workers=20) as executor:
+            future_to_url = {executor.submit(get_redirect_url, url): url for url in ["https://en.wikipedia.org/wiki/Special:Random"] * total}
+            for i, future in enumerate(future_to_url):
+                final_url = future.result()
+                links.append(final_url)
+        # Save Links
+        with open(cache_file, "w") as f:
+            for link in links:
+                f.write(link + "\n")
+        
+    download_pages(cache_file, current_dir + "/raw_not_hotel")
+
+grab_random_wiki_article()
